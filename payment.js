24e1eb850fa136918a0c9da4527a8b56b7a10c3d
Sajeeb <62443848+sajeeb007@users.noreply.github.com>
2021-01-24 20:23:48 +0600

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,17 @@
+var id_token = '';
+$.ajax({
+    url: 'bkash.html/tokenized/checkout/token/grant',
+    type: 'POST',
+    contentType: 'application/json',
+    beforeSend: function(tokenRequest) {
+        tokenRequest.setRequestHeader("username", "shared_name");
+        tokenRequest.setRequestHeader("password", "shared_password");
+    },
+    data: JSON.stringify( { "app_key": "shared_app_key", "app_secret": "shared_app_secret" } ),
+    success: function(data) {
+    if (data && data.id_token != null) {
+      id_token = data.id_token;
+      $('#bKash_button').removeAttr('disabled');
+    }
+  }
+});
